dfa31b6dc852bd7e6d5035893aac9620234a4452
mohamed199 <moh246samy@gmail.com>
2026-06-08 17:26:07 +0300

    updated

--- src/seed.js
+++ src/seed.js
@@ -116,7 +116,7 @@ const seedDoctor = async (distributor, areaManager) => {
     phone:       "06 98765432",
     areaManager: areaManager._id,
     distributor: distributor._id,
-    agency:     "—",
+    agency:     "",
   });
 
   console.log("✅ Sample doctor created:");
